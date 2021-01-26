a94d95ff45e0284be6484c56e2b47cae7eba3f74
Konstantin Dmitrenko <werhawer@gmail.com>
2021-01-26 15:34:56 +0200

    change logo to img

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,3 +1,9 @@
-const Logo = () => <div className="logo"></div>;
+import logoSrc from "public/image/logo.svg";
+
+const Logo = () => (
+  <div className="logo">
+    <img src={logoSrc} alt="logo" width="88" height="88" />
+  </div>
+);
 
 export default Logo;
